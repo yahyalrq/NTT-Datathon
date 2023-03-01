fc87824941654f2caa4bc8acf47de65268cca0a4
yahyalrq <84201630+yahyalrq@users.noreply.github.com>
2023-03-01 16:03:49 +0100

    changes

--- utils.py
+++ utils.py
@@ -8,16 +8,16 @@ import pickle
 
 @st.cache(show_spinner=False)
 def load_dataset():
-    return pd.read_csv(r'finaldataset.csv')
+    return pd.read_csv(r'data/finaldataset.csv')
 
 @st.cache(show_spinner=False)
 def process_dataset(df):
     df['InspectionDate'] = pd.to_datetime(df['InspectionDate'])
     df['InspectionYear'] = df['InspectionDate'].dt.year
     df["repairingcostsaved"]=df["repairingcostassumed"]-df["prepairingcostassumed"]
-    df.loc[df['repairingcostsaved'] < 0, 'repairingcostsaved'] = 0
+    #df.loc[df['repairingcostsaved'] < 0, 'repairingcostsaved'] = 0
     df["Lossineurossaved"]=df["Lossineuros"]-df["pLossineuros"]
-    df.loc[df['Lossineurossaved'] < 0, 'Lossineurossaved'] = 0
+    #df.loc[df['Lossineurossaved'] < 0, 'Lossineurossaved'] = 0
     df["carbonfootprintsaved"]=df["carbonfootprintemitted"]-df["pcarbonfootprintemitted"]
     #df.loc[df['carbonfootprintsaved'] < 0, 'carbonfootprintsaved'] = 0
     return df
